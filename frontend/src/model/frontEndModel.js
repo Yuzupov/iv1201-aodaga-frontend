51effe70e014618d0bd696b98db114c79b010009
Alexander Rocabado Hellgren <alrh@kth.se>
2025-01-31 13:06:10 +0100

    first variant of registering a user

--- frontend/src/model/frontEndModel.js
+++ frontend/src/model/frontEndModel.js
@@ -6,6 +6,7 @@ export default {
 	userName: "",
 	userPassword: "",
 	confirmUserPassword: "",
+	confirmationMessage: "",
 
 	setField(props){
 		/*this method is intended to set the different fields such as firstName, lastName etc. through the use of a function instead of setting it raw. It will require that the presenter has a "field" and "field.value" naming convention. Otherwise the names of the variables below will need to be changed to match what is in the presenter
@@ -20,5 +21,35 @@ export default {
 		}
 		*/
 		this[props.field] = props.field.value;
+	},
+	registerUser = async () => {
+		try{
+			const response = await fetch(
+				'localhost:4567/register',
+				{
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						firstName: this.firstName, 
+						lastName: this.lastName, 
+						email: this.email, 
+						personalNumber: this.personalNumber 
+						userName: this.userName, 
+						userPassword: this.userPassword, 
+						confirmUserPassword: this.confirmUserPassword, 
+
+					}),
+				}
+			);
+			if(!response.ok){
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			const data = await response.json();
+			console.log(`Returned: ${data}`);
+			return data;
+		} catch (error) {
+			console.error(`Error when registering: ${error}`);
+			throw error;
+		}
 	},
 }
