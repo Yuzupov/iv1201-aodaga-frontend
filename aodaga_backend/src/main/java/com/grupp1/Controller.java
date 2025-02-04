63508df0069b2db7e4b321f9ff1de3ceb36473cd
Jonathan Johnson <jjohnso@kth.se>
2025-02-04 15:10:46 +0100

    Added checking for pnr and email

--- aodaga_backend/src/main/java/com/grupp1/Controller.java
+++ aodaga_backend/src/main/java/com/grupp1/Controller.java
@@ -3,7 +3,6 @@ package com.grupp1;
 import com.grupp1.api.BadApiInputException;
 import com.grupp1.api.ServerException;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 public class Controller {
@@ -21,13 +20,28 @@ public class Controller {
     String[] parsedFields = new String[6];
     int i = 0;
     for (String field : expectedFields) {
-      String f = (String) fields.get(field);
       if (fields.get(field) == null) {
         throw new BadApiInputException("missing '" + field + "' parameter");
-      } else if (fields.get(field).getClass() != String.class) {
+      }
+      if (fields.get(field).getClass() != String.class) {
         throw new BadApiInputException("bad '" + field + "' parameter (should be string)");
-      } else if (f.length() > 255) {
+      }
+      String f = (String) fields.get(field);
+      if (f.length() > 255) {
         throw new BadApiInputException("'" + field + "' too long");
+      }
+      if (field.equals("personalNumber")) {
+        if (!f.matches("\\d{8}-\\d{4}")) {
+          throw new BadApiInputException("Invalid 'personalNumber' format");
+        }
+      }
+      if (field.equals("email")) {
+        //RFC 5322
+        if (!f.matches(
+            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+          throw new BadApiInputException("Invalid '" + field + "' format");
+        }
+
       }
       parsedFields[i++] = f;
     }
