3dce2e84fb076b2e6e1e7c86066ce1c25b9c70eb
Jonathan Johnson <jjohnso@kth.se>
2025-02-05 11:19:31 +0100

    DB.createUser now gives new user the correct role

--- aodaga_backend/src/main/java/com/grupp1/DB.java
+++ aodaga_backend/src/main/java/com/grupp1/DB.java
@@ -18,11 +18,14 @@ class DB {
   static void createUser(String name, String surname, String pnr, String email, String password,
       String username)
       throws SQLException {
-    String query = "INSERT INTO person (name, surname, pnr, email, password, username) " +
-        "VALUES (?, ?, ?, ?, ?, ?)";
+    String query = "INSERT INTO person (name, surname, pnr, email, password, username, role_id) " +
+        "VALUES (?, ?, ?, ?, ?, ?, ?)";
     try {
-
       Connection conn = getConn();
+      Statement lol = conn.createStatement();
+      ResultSet rs = lol.executeQuery("SELECT role_id FROM role WHERE name = 'applicant'");
+      rs.next();
+      int role_id = rs.getInt("role_id");
       PreparedStatement stmt = conn.prepareStatement(query);
       stmt.setString(1, name);
       stmt.setString(2, surname);
@@ -30,12 +33,9 @@ class DB {
       stmt.setString(4, email);
       stmt.setString(5, password);
       stmt.setString(6, username);
-
+      stmt.setInt(7, role_id);
       System.out.println(query);
-      if (!stmt.execute()) {
-        throw new SQLException("Unknown SQL Error");
-      }
-
+      stmt.execute();
       conn.close();
     } catch (SQLException e) {
       e.printStackTrace();
