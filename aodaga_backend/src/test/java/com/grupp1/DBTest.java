154bfd0439bd77d5fd4c17d238974dd2be25dc65
David <davidhae@kth.se>
2025-02-06 10:52:33 +0100

    Tests, not done but weekend-merge time.

---
+++ aodaga_backend/src/test/java/com/grupp1/DBTest.java
@@ -0,0 +1,17 @@
+package com.grupp1;
+
+import junit.framework.TestCase;
+
+public class DBTest extends TestCase {
+
+  /**
+   * What do we want to test here?
+   * we want to see that if the correct strings are supplied, a user is created.
+   * we want to test what happens if the strings are all empty
+   * we want to see test what happens if the strings are Null
+   *
+   */
+  public void testCreateUser() {
+  }
+
+}
--- aodaga_backend/src/test/java/com/grupp1/api/APITest.java
+++ aodaga_backend/src/test/java/com/grupp1/api/APITest.java
@@ -1,11 +1,45 @@
 package com.grupp1.api;
 
+import java.util.HashMap;
 import junit.framework.TestCase;
 
 public class APITest extends TestCase {
 
+  @org.junit.jupiter.api.Test
+  void register() {
+  }
+
+  private class FakeRegisterObject{
+
+    String body(){
+      return  "{"
+          +"\"firstName\" : \"feta\","
+          +"\"lastName\" : \"ost\","
+          +"\"email\" : \"feta@ost.com\","
+          +"\"personalNumber\" : \"12355\","
+          +"\"userName\" : \"feta\","
+          +"\"UserPassword\" : \"ost\","
+          +"\"confirmUserPassword\" : \"\""
+          +"}";
+    }
+  }
 
   public void testRegister() {
-
+    FakeRegisterObject req = new FakeRegisterObject();
+    API api = new API(4567);
+    //assertEquals("woop woop", api.register(req));
   }
 }
+
+/*
+    "{"
+          +"\"firstName\" : \"feta\","
+          +"\"lastName\" : \"ost\","
+          +"\"email\" : \"feta@ost.com\","
+          +"\"personalNumber\" : \"12355\","
+          +"\"userName\" : \"feta\","
+          +"\"UserPassword\" : \"ost\","
+          +"\"confirmUserPassword\" : \"\""
+          +"}";
+
+    */
