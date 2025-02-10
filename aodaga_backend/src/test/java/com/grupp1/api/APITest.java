1438bdf8a06ccb59087bdf2a60e822c0cadcb90d
David <davidhae@kth.se>
2025-02-10 13:17:10 +0100

    Tests basics.

--- aodaga_backend/src/test/java/com/grupp1/api/APITest.java
+++ aodaga_backend/src/test/java/com/grupp1/api/APITest.java
@@ -1,12 +1,19 @@
 package com.grupp1.api;
 
 import java.util.HashMap;
+import org.junit.Before;
 import junit.framework.TestCase;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 public class APITest extends TestCase {
 
   @org.junit.jupiter.api.Test
   void register() {
+  }
+
+  @Before
+  public void setUp() {
   }
 
   private class FakeRegisterObject{
@@ -25,6 +32,7 @@ public class APITest extends TestCase {
   }
 
   public void testRegister() {
+
     FakeRegisterObject req = new FakeRegisterObject();
     API api = new API(4567);
     //assertEquals("woop woop", api.register(req));
@@ -37,7 +45,7 @@ public class APITest extends TestCase {
           +"\"lastName\" : \"ost\","
           +"\"email\" : \"feta@ost.com\","
           +"\"personalNumber\" : \"12355\","
-          +"\"userName\" : \"feta\","
+          +"\"username\" : \"feta\","
           +"\"UserPassword\" : \"ost\","
           +"\"confirmUserPassword\" : \"\""
           +"}";
