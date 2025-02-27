9e6ef17e39aa4df00d20e6e0c0b00a198fb7656f
Alexander Rocabado Hellgren <alrh@kth.se>
2025-02-27 14:59:02 +0100

    fixed some cookie stuff in presenter and model

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,28 @@ import DashboardView from "./view/dashboardView";
 import CreateResetLinkView from "./view/createResetLinkView";
 import ResetLinkView from "./view/resetView";
 
+function getCookie(cookieName){
+	const cookies = document.cookie.split("; ");
+	for (let cookie of cookies) {
+		let [key, value] = cookie.split("=");
+		if (key === cookieName) {
+			return JSON.parse(decodeURIComponent(value));
+		}
+	}
+	return "";
+}
+
 const ProtectedRoute = ({children}) => {
-	const userToken = document.cookie;
+	const userToken = document.cookie
 	return userToken ? children : <Navigate to="/login" replace/>;
+}
+
+const ProtectedRouteList = ({children}) => {
+	console.log(children);
+	const userToken = getCookie("loginCookie");
+	console.log("In App.jsx");
+	console.log(userToken);
+	return userToken.userRole === "recruiter" ? children : <Navigate to="/dashboard" replace/>;
 
 }
 const IfTokenLogin = ({children}) => {
@@ -20,28 +39,27 @@ const IfTokenLogin = ({children}) => {
 }
 
 const Recruiter = ({children}) => { // maybe add this to the view for conditionald rendering. Show buttton if recruiter only 
-  const recruiterToken = 0; // add recruiter variable 
-  return !recruiterToken ? children : <Navigate to="/"/>;
+	const recruiterToken = 0; // add recruiter variable 
+	return !recruiterToken ? children : <Navigate to="/"/>;
 }
 
 const App = () => {
-  
-  return (
-  <Router>
-    <Routes>
-      <Route path="/" element={<ProtectedRoute><DashboardView /></ProtectedRoute> } />
-      <Route path="/start" element={<StartView />} />
-      <Route path="/create-account" element={ <CreateView /> } />
-      <Route path="/reset/create-link" element={ <CreateResetLinkView/> } />
-      <Route path="/reset/:token" element={ <ResetLinkView/> } />
-      <Route path="/list-applicants" element={ <ListApplicationsView /> } />
-      <Route path="/login" element={<IfTokenLogin><LoginView /></IfTokenLogin>} /> 
-      <Route path="/dashboard" element={<ProtectedRoute><DashboardView /></ProtectedRoute> } />
-      <Route path="/applicants" element={<ProtectedRoute><ListApplicationsView /></ProtectedRoute>} />
-      <Route/>
-    </Routes>
-  </Router>
-  );
+
+	return (
+		<Router>
+		<Routes>
+		<Route path="/" element={<ProtectedRoute><DashboardView /></ProtectedRoute> } />
+		<Route path="/start" element={<StartView />} />
+		<Route path="/create-account" element={ <CreateView /> } />
+		<Route path="/reset/create-link" element={ <CreateResetLinkView/> } />
+		<Route path="/reset/:token" element={ <ResetLinkView/> } />
+		<Route path="/login" element={<IfTokenLogin><LoginView /></IfTokenLogin>} /> 
+		<Route path="/dashboard" element={<ProtectedRoute><DashboardView /></ProtectedRoute> } />
+		<Route path="/applicants" element={<ProtectedRouteList><ListApplicationsView /></ProtectedRouteList>} />
+		<Route/>
+		</Routes>
+		</Router>
+	);
 };
 
 export default App;
