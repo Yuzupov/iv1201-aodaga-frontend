6dad960a268138e9a9da35c7f32f271ff02ecc91
Alexander Rocabado Hellgren <alrh@kth.se>
2025-03-06 17:24:13 +0100

    updated error handling

--- src/presenter/loginPresenter.jsx
+++ src/presenter/loginPresenter.jsx
@@ -21,35 +21,10 @@ const LoginPresenter = {
       }
       
       const response = await frontEndModel.login(formData);
-   // 
-   //   if (!response || !response.userPassword || !response.username )
-   //         {
-   //     throw new Error("User not found");
-   //   }
-   //    if (formData.userPassword !== response.userPassword && formData.username !== response.username || formData.email !== response.email) {
-   //      throw new Error("Invalid credentials");
-   //    }
-      // if (formData.userPassword !== response.userPassword || formData.username !== response.username) {
-      //   throw new Error("Invalid credentials");
-      // }
-      // if (isEmail){
-      //   if (formData.username !== response.email) {
-      //     throw new Error("Incorrect email");
-      //   }
-      //   else {
-      //     if (formData.username !== response.username) {
-      //       throw new Error("Incorrect username");
-      //     }
-      //     if (formData.userPassword !== response.userPassword) {
-      //       throw new Error("Incorrect password");
-      //     }
-      //   }
-      // }
-      onSuccess(response); 
+      onSuccess(response);
     } catch (error) {
-      console.error("Error in LoginPresenter:", error.message);
-      //onError(error.message);
-      throw error;
+      onError(error.message);
+      console.log(error.message);
     }
   },
 };
